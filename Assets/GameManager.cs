417cc7d2847f5354cdd361024ac4a6be2128d3cd
bimorefkaadiriskisaputratik24-maker <bimo.refka.adiriski.saputra.tik24@stu.pnj.ac.id>
2026-04-03 11:52:34 +0700

    game pong update empat (final)

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,32 +1,65 @@
 using UnityEngine;
-using TMPro; // Kita pakai TextMeshPro biar teksnya tajam
+using TMPro; 
 
 public class GameManager : MonoBehaviour
 {
-private int skorKiri = 0;
-private int skorKanan = 0;
+    private int skorKiri = 0;
+    private int skorKanan = 0;
+    private int batasMenang = 11; // Syarat menang di poin 11
 
-[SerializeField] private TextMeshProUGUI teksSkorKiri;
-[SerializeField] private TextMeshProUGUI teksSkorKanan;
-    // Fungsi untuk nambah skor kiri
+    [SerializeField] private TextMeshProUGUI teksSkorKiri;
+    [SerializeField] private TextMeshProUGUI teksSkorKanan;
+    
+    // TAMBAHAN: Kita bikin slot buat masukin script bola ke sini
+    [SerializeField] private Padel_Ball scriptBola; 
+
+    void Start()
+    {
+        // PENTING: Kembalikan waktu berjalan normal (1) setiap kali game di-play.
+        Time.timeScale = 1f; 
+    }
+
     public void SkorKiriBertambah()
     {
         skorKiri++;
-        teksSkorKiri.text = skorKiri.ToString();
-        ResetBola();
+        
+        if (skorKiri >= batasMenang)
+        {
+            teksSkorKiri.text = "WIN";
+            teksSkorKanan.text = "LOSE";
+            Time.timeScale = 0f; // Bekukan waktu game (Game Over)
+        }
+        else
+        {
+            teksSkorKiri.text = skorKiri.ToString();
+            
+            // Panggil fungsi ResetBall yang ada di script Padel_Ball
+            if(scriptBola != null) 
+            {
+                scriptBola.ResetBall(); 
+            }
+        }
     }
 
-    // Fungsi untuk nambah skor kanan
     public void SkorKananBertambah()
     {
         skorKanan++;
-        teksSkorKanan.text = skorKanan.ToString();
-        ResetBola();
-    }
-
-    void ResetBola()
-    {
-        // Kodingan yang benar
-        GameObject.Find("Circle").GetComponent<Padel_Ball>().ResetBall();
+        
+        if (skorKanan >= batasMenang)
+        {
+            teksSkorKanan.text = "WIN";
+            teksSkorKiri.text = "LOSE";
+            Time.timeScale = 0f; // Bekukan waktu game (Game Over)
+        }
+        else
+        {
+            teksSkorKanan.text = skorKanan.ToString();
+            
+            // Panggil fungsi ResetBall yang ada di script Padel_Ball
+            if(scriptBola != null) 
+            {
+                scriptBola.ResetBall(); 
+            }
+        }
     }
 }
